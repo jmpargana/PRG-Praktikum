4eec4410c2ec0c01afc7b6aa0fd40b75d39f8063
jmpargana <jmpargana@stud.uni-frankfurt.de>
2019-10-20 21:59:06 +0200

    cellular automaton header

---
+++ Milestones/Milestone1/code/include/cellular_automaton.h
@@ -0,0 +1,90 @@
+/**
+ * PRG-Prakitkum WS19/20
+ *
+ * Author: Joao Pargana - 6807391
+ * Gruppe: 3
+ *
+ * Aufgabe1 header file for c) d) and e) containing the cellular automaton class
+ *
+ */
+
+
+#include <iostream>
+#include <fstream>
+
+
+//------------------------------------------------------------------------------
+
+
+template<typename T>
+class CellularAutomaton {
+public:
+
+    class Proxy {
+    public:
+	Proxy(T* _array) : array{_array} { }
+	T& operator[](int index) { return array[index]; }
+	
+    private:
+	T* array;
+    };
+
+    
+    // constructors
+    CellularAutomaton()		// default constructor
+	: current{new T[30][30]}, next{new T[30][30]}, size{30} { } 
+    CellularAutomaton(const int _size)
+	: current{new T[_size][size]}, next{new T[_size][size]}, size{_size} { }
+
+    // copy constructor
+    CellularAutomaton(CellularAutomaton&);
+
+    // move constructor
+    
+
+    // assignment operator
+    CellularAutomaton& operator=(const CellularAutomaton&);
+
+    // move assignment operator
+    CellularAutomaton& operator=(CellularAutomaton&&) noexcept;
+
+    // destructors
+    ~CellularAutomaton() { delete[] next; delete[] current; }
+
+    // time lapse with operator++ overloading 
+    CellularAutomaton& operator++();
+
+    // subscript operator for user
+    Proxy operator[](int index) { return Proxy(current[index]); }
+    
+    // getter functions with module
+    
+    
+    
+private:
+    T** current;
+    T** next;
+    int size;
+    
+};
+
+
+//------------------------------------------------------------------------------
+
+
+// output stream
+template<typename T>
+std::ostream& operator<<(std::ostream&, const CellularAutomaton<T>&);
+
+// input stream
+template<typename T>
+std::istream& operator>>(std::istream&, CellularAutomaton<T>&);
+
+
+// output file stream
+template<typename T>
+std::ofstream& operator<<(std::ostream&, const CellularAutomaton<T>&);
+
+// input file stream
+template<typename T>
+std::ifstream& operator>>(std::ifstream&, CellularAutomaton<T>&);
