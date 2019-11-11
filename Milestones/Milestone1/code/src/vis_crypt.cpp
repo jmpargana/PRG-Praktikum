2f07074806442fd5c4fa22ad8e1e99578c438109
klezm <jjosh+github@live.de>
2019-11-11 14:45:00 +0100

    visCrypt > export

--- Milestones/Milestone1/code/src/vis_crypt.cpp
+++ Milestones/Milestone1/code/src/vis_crypt.cpp
@@ -14,7 +14,10 @@
 XBild::XBild() {}
 
 // create 2D Matrix with x rows filled with y columns each and all elements set to false
-XBild::XBild(unsigned int x, unsigned int y) : imageMx{x, std::vector<bool>(y, false)} { xLen = x; yLen = y; }
+XBild::XBild(unsigned int x, unsigned int y) : imageMx{x, std::vector<bool>(y, false)} {
+    xLen = x;
+    yLen = y;
+}
 //XBild::XBild(unsigned int x, unsigned int y) {
 //    imageMx.resize(x);
 //    for (unsigned row = 0; row < imageMx.size(); row++) {
@@ -37,7 +40,6 @@ XBild &XBild::operator=(XBild &&other) noexcept {
     imageMx = std::move(other.imageMx);
     return *this;
 }
-
 
 
 // read & write
@@ -66,6 +68,7 @@ const std::vector<bool> &XBild::operator[](unsigned col) const
 // unsigned XBild::get_x_size() const { return imageMx.size(); }
 // unsigned XBild::get_y_size() const { return imageMx[0].size(); }
 unsigned XBild::get_x_size() const { return this->xLen; }
+
 unsigned XBild::get_y_size() const { return this->yLen; }
 
 // matrix construct to build x*y matrix for the image
@@ -91,82 +94,28 @@ void XBild::printImage() {
     }
 }
 
+// https://codereview.stackexchange.com/questions/206852/template-matrix-class-second-version
 void XBild::importFile(std::string file) {
     std::ifstream isf(file);
     if (!isf.is_open()) throw std::runtime_error("io error while reading: file not found.");
 
     char elem;
-    // const char* txt;
-    std::string lines = "";
+    std::string lines; // = "";
+    std::vector<bool> elems; // (xLen, false); //(xLen, false);
 
-    // std::vector<bool> elems (xLen, false); //(xLen, false);
-    std::vector<std::vector<bool>> elemns; // (yLen, elems);
+    imageMx.clear(); // this->imageMx.clear();
 
-
-    XBild tmp(xLen, yLen);
-
-    tmp.imageMx.clear();
-    // this->imageMx.clear();
-    imageMx.clear();
-    // elems.clear();
-    // elemns.clear();
-
-    // elems.resize(xLen);
-    // elemns.push_back(elems);
-    // elemns.assign(2, elems);
     while (std::getline(isf, lines)) {
-        std::vector<bool> elems; // (xLen, false); //(xLen, false);
-        // std::istringstream lineStream(lines);
-        std::stringstream lineStream(lines);
-        // tmp.imageMx.resize(tmp.imageMx.size() + 1);
-        // elemns.resize(elemns.size() + 1);
-        while (lineStream >> elem)
-        {
-            elems.push_back(elem == '1');
-            std::cout << elem;
+        elems.clear(); // std::vector<bool> elems;
+        std::stringstream lineStream(lines); // std::istringstream lineStream(lines);
+        while (lineStream >> elem) {
+            elems.push_back(elem == '1' || elem == 'A');
         }
-        lineStream.clear();
-        tmp.imageMx.push_back(elems);
-        // elemns[elemns.size()-1].push_back(elems);
-        elemns.push_back(elems);
+        // lineStream.clear();
         imageMx.push_back(elems);
-        std::cout << std::endl;
     }
-    std::cout << std::endl;
 
-//     for (unsigned row = 0; row < xLen; row++) {
-//         elems.clear();
-//         elems.assign(xLen, false);
-//         // elems.resize(xLen, 0);
-//         txt = "";
-//         for (unsigned col = 0; col < yLen; col++) {
-//             // isf >> tmp.imageMx[row][col];
-//             isf >> elem;
-//             txt += elem;
-//             // std::cout << elem << std::endl;
-// //            tmp[row][col] = (elem == '1' || elem == 'A');
-//             // tmp.imageMx[row][col] = (elem == '1' || elem == 'A');
-//             if ((elem == '1') || (elem == 'A')) {
-//                 // elems.push_back(true);
-//                 elems[col] = true;
-//             }
-//             else {
-//                 // elems.push_back(false);
-//                 elems[col] = false;
-//             }
-//         }
-//         tmp.imageMx.push_back(elems);
-//         // std::cout << txt << std::endl;
-//         // std::cout << &txt << std::endl; // memory address
-//         // std::cout << *txt << std::endl;
-//     }
     isf.close();
-    tmp.printImage();
-
-    // this->imageMx = tmp.imageMx;
-    // imageMx = tmp.imageMx;
-    // this->imageMx = elemns;
-    // imageMx = elemns;
 
     std::cout << "import successful" << std::endl;
     std::cout << "x: " << xLen << " y: " << yLen << std::endl;
@@ -175,8 +124,15 @@ void XBild::importFile(std::string file) {
 void XBild::exportFile(std::string file) {
     std::ofstream osf{file};
     if (!osf) throw std::runtime_error("io error while writing: file not found.");
-    //TODO: write image to file
-    std::cout << "export successful" << std::endl;
+    // https://stackoverflow.com/questions/409348/iteration-over-stdvector-unsigned-vs-signed-index-variable/409396#409396
+    for (auto const &line: imageMx) {
+        for (auto const &elem: line) {
+            osf << elem;
+        }
+        osf << std::endl;
+    }
+    osf.close();
+    std::cout << "export successful: " << file << std::endl;
 }
 
 // task 2c
@@ -203,6 +159,7 @@ int main(int argc, const char **argv) {
 //    imEncoded.importFile("../../materials/beispielbild_1.txt");
     imEncoded.importFile("../materials/beispielbild_1.txt");
 //    imEncoded.importFile("Milestones/Milestone1/materials/beispielbild_1.txt");
+    imEncoded.exportFile("../materials/beispielbild_1_OUT.txt");
 
     // std::cout << imEncoded.printImage() << std::endl;
     imEncoded.printImage();
