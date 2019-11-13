7c4d6f8d8737a1f9748ae2d8e616af0299136475
jmpargana <jmpargana@stud.uni-frankfurt.de>
2019-11-13 19:33:10 +0100

    preparation for milestone 2

---
+++ Milestones/Milestone2/code/src/neuron.cpp
@@ -0,0 +1,255 @@
+/**
+ * PRG-Prakitkum WS19/20
+ *
+ * Author: Joao Pargana - 6807391
+ * Gruppe: 3
+ *
+ * neuron.cpp source file with all declarations for neuron class
+ *
+ */
+
+
+#include "../include/neuron.hpp"
+
+
+//------------------------------------------------------------------------------
+
+
+/**
+ * Default constructor for class 
+ * Assigns the default activation function
+ */
+Neuron::Neuron()
+{
+    // activation_function = [](int, int){std::accumulate(inputs.begin(), inputs.end(), 0);};
+}
+
+
+//------------------------------------------------------------------------------
+
+
+/* 
+ * Constructor for square matrix with given size
+ * 
+ * @param other
+ * @param first
+ * @param second
+ * 
+ */
+Neuron::Neuron(std::function<void(int first, int second)> other)
+{
+    activation_function = other;
+}
+
+
+//------------------------------------------------------------------------------
+
+
+/**
+ * Copy constructor
+ * 
+ * @param other is another instance of the same class
+ * 
+ */
+Neuron::Neuron(Neuron& other) :
+    activation_function{other.activation_function}, inputs{other.inputs}  { }
+
+
+//------------------------------------------------------------------------------
+
+
+/** 
+ * Copy constructor for reference
+ * 
+ * @param other is another instance of same class
+ * 
+ */
+Neuron::Neuron(Neuron&& other) :
+    activation_function{std::move(other.activation_function)},
+    inputs{std::move(other.inputs)}  { }
+
+
+//------------------------------------------------------------------------------
+
+
+/**
+ * Assignment operation
+ * 
+ * @param lhs is another instance of the same class
+ * 
+ */
+Neuron& Neuron::operator=(const Neuron& lhs)
+{
+    activation_function = lhs.activation_function;
+    inputs = lhs.inputs;
+    return *this;
+}
+
+
+//------------------------------------------------------------------------------
+
+
+/**
+ * Assignment operation with reference
+ * 
+ * @param lhs is another instance of the same class
+ * 
+ */
+Neuron& Neuron::operator=(Neuron&& lhs) noexcept
+{
+    activation_function = std::move(lhs.activation_function);
+    inputs = std::move(lhs.inputs);
+    return *this;
+}
+
+
+//------------------------------------------------------------------------------
+
+
+/**
+ * Default destructor
+ * doesn't need any special instructions, since pointer to lambda function 
+ * and vector already have their default destructors to be called
+ * 
+ */
+Neuron::~Neuron() { }
+
+
+//------------------------------------------------------------------------------
+
+
+/**
+ * Assign a value of inputs with their correspondent weights to the
+ * inputs variable of the Neuron
+ * 
+ * @param values contains vector of pair of doubles (inputs and weights)
+ * @return void
+ * 
+ */
+Neuron& Neuron::set_inputs(std::vector<std::pair<double, double>>& values)
+{
+    inputs = values;
+    return *this;
+}
+
+
+//------------------------------------------------------------------------------
+
+
+/**
+ * Setter function for activation lambda function
+ *  
+ * @param first
+ * @param second
+ * @param other new lambda function
+ * 
+ * @return void
+ * 
+ */
+Neuron& Neuron::set_activation_function(std::function<void(int first, int second)> other)
+{
+    this->activation_function = other;
+    return *this;
+}
+
+
+//------------------------------------------------------------------------------
+
+
+/**
+ * Getter function for inputs attribute that can assign
+ * @return vector of inputs and weights
+ */
+std::vector<std::pair<double, double>>& Neuron::get_inputs()
+{
+    return this->inputs;
+}
+
+
+//------------------------------------------------------------------------------
+
+
+/**
+ * Getter function for const values only for reading
+ * @return vector of inputs and weights
+ */
+const std::vector<std::pair<double, double>>& Neuron::get_inputs() const
+{
+    return this->inputs;
+}
+
+
+//------------------------------------------------------------------------------
+
+
+/**
+ * Output stream operator overload 
+ * 
+ * @param os is an instance of the standard output stream class
+ * @param lhs is an instance of the Neuron class
+ * 
+ * @return the same output stream
+ * 
+ */
+std::ostream& operator<<(std::ostream& os, Neuron const& lhs)
+{
+    
+    return os;
+}
+
+
+//------------------------------------------------------------------------------
+
+
+/** 
+ * Input stream operator overload
+ * 
+ * @param is is an instance of the standard input stream class
+ * @param lhs is an instance of the CellularAutomaton class
+ * 
+ * @return the same input stream
+ * 
+ */
+std::istream& operator>>(std::istream& is, Neuron& lhs)
+{
+
+    return is;
+}
+
+
+//------------------------------------------------------------------------------
+
+
+/**
+ * Output file stream operator overload
+ * 
+ * @param os is an instance of the standard output file stream 
+ * @param lhs is an instance of the CellularAutomaton class
+ * 
+ * @return the same output file stream
+ * 
+ */
+std::ofstream& operator<<(std::ofstream& os, Neuron const& lhs)
+{
+
+    return os;
+}
+
+
+//------------------------------------------------------------------------------
+
+
+/**
+ * Input file stream operator overload
+ * 
+ * @param is is an instance of the standard input file stream
+ * @param lhs is an instance of the CellularAutomaton class
+ * 
+ * @return the same input file stream
+ * 
+ */
+std::ifstream& operator>>(std::ifstream& is, Neuron& lhs)
+{
+
+    return is;
+}
