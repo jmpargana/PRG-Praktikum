706eca16ec3f9a766cf3b06c13d5a42af756d04e
klezm <jjosh+github@live.de>
2019-12-15 23:48:22 +0100

    controller paste & copy from fcnn

--- Milestones/Milestone2/code/include/controller.h
+++ Milestones/Milestone2/code/include/controller.h
@@ -16,10 +16,10 @@ public:
     Controller();
     ~Controller();
 
+    boost::numeric::ublas::matrix<double> import_file(std::string);
+    void batch_normalization(unsigned, std::vector<std::vector<boost::filesystem::directory_entry>>&)
     void parse_directory(const char*, std::vector<boost::filesystem::directory_entry>);
     void run_epoch(unsigned, unsigned, unsigned);
-    void batch_normalization(unsigned, std::vector<std::vector<boost::filesystem::directory_entry>>&)
-    boost::numeric::ublas::matrix<double> import_file(std::string);
 
     void setTypology(unsigned, unsigned, unsigned);
     void setSplit(unsigned, unsigned);
